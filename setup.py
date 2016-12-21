633d3af767e4d09670d311bcf4d1cdb6a6d1c1f2
Gerard Duval <gduvalsc@gmail.com>
2016-12-21 19:15:54 +0100

    ajout script kairos

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='pykairos',
+      version='3.0',
+      description='Python aiohttp server used by Kairos',
+      author='Gerard Duval',
+      author_email='gerard.duval@gdsoftconsulting.com',
+      url='https://github.com/gduvalsc/kairos',
+      license='GPL',
+      packages=['pykairos'],
+      classifiers=[ "Development Status :: 3 - Alpha", "Topic :: Utilities", "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)"],
+      zip_safe=False)
