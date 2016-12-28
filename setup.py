62e2af61d71c18bc1efe3dfcc38db4c0b3bb7e6c
Gerard Duval <gduvalsc@gmail.com>
2016-12-28 17:33:20 +0100

    Development status change

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='pykairos',
       url='https://github.com/gduvalsc/kairos',
       license='GPL',
       packages=['pykairos'],
-      classifiers=[ "Development Status :: 3 - Alpha", "Topic :: Utilities", "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)"],
+      classifiers=[ "Development Status :: 5 - Production/Stable", "Topic :: Utilities", "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)"],
       zip_safe=False)
