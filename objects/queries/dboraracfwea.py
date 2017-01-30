070d90cdb92020ab673054399e0b43870ae03e93
Gerard Duval <gduvalsc@gmail.com>
2017-01-30 19:27:13 +0100

    a few bugs in objects ...

--- objects/queries/dboraracfwea.py
+++ objects/queries/dboraracfwea.py
@@ -1,11 +1,11 @@
 class UserObject(dict):
     def __init__(s):
-        if "DBORARACFWES" not in kairos: kairos['DBORARACFWES']=''
+        if "DBORARACFWEA" not in kairos: kairos['DBORARACFWEA']=''
         object = {
             "type": "query",
-            "id": "DBORARACFWES",
+            "id": "DBORARACFWEA",
             "collection": "DBORARACTTFE",
             "nocache": True,
-            "request": "select timestamp, 'All instances' label, sum(timewaited) value from DBORARACTTFE where inum=0 and event = '" + kairos["DBORARACFWES"] + "' group by timestamp, label order by timestamp"
+            "request": "select timestamp, 'All instances' label, sum(timewaited) value from DBORARACTTFE where inum=0 and event = '" + kairos["DBORARACFWEA"] + "' group by timestamp, label order by timestamp"
         }
         super(UserObject, s).__init__(**object)
